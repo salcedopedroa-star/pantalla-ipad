6dd5619a4cd4bdc9fa659a4e9f3f46bb818c51b4
salcedopedroa-star <salcedopedroa@gmail.com>
2026-05-29 12:35:13 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+// CONFIGURACION
+// Si regeneras tu clave en JSONBin, solo cambia el valor de "masterKey" aqui.
+const CONFIG = {
+  binId: "6a19664921f9ee59d2990d6d",
+  masterKey: "$2a$10$aIvWm7EpciOE1ggc8IP.pu0Q/aLQ3naKaD8p1UL9h4RlyPzGJdmFa"
+};
