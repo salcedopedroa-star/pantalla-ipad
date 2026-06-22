c27c363043252344d6ccb2fac7ca8f55ade38411
salcedopedroa-star <salcedopedroa@gmail.com>
2026-06-22 10:19:55 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,11 @@
-// CONFIGURACION
-// Esta version usa GitHub Gists como buzon, no tiene limite mensual.
-const CONFIG = {
-  gistId: "086b5254b5652ccb8e6cc77e37feb6c7",
-  token: "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",
-  fileName: "estado.json"
+// CONFIGURACION FIREBASE
+// La apiKey NO es secreta, Firebase la disena para ser publica.
+const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyBVTVXzM5_P-2FKaISrQlyZFFJLOLxJJsc",
+  authDomain: "pantalla-ipad.firebaseapp.com",
+  databaseURL: "https://pantalla-ipad-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "pantalla-ipad",
+  storageBucket: "pantalla-ipad.firebasestorage.app",
+  messagingSenderId: "783473930843",
+  appId: "1:783473930843:web:b379a25c813e4d7ef1c57e"
 };
