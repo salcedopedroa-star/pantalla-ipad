94c63f9eb7d073e1e9d7b1c79376121f1d0ff324
salcedopedroa-star <salcedopedroa@gmail.com>
2026-06-22 09:38:37 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // CONFIGURACION
-// Si en el futuro tienes que renovar la cuenta de JSONBin,
-// solo cambia el valor de "masterKey" y opcionalmente "binId" aqui.
+// Esta version usa GitHub Gists como buzon, no tiene limite mensual.
 const CONFIG = {
-  binId: "6a2fb659f5f4af5e29f3dba6",
-  masterKey: "$2a$10$JrADpBkvXk0FqdxxPVQmDuVBH52abejRFo9dHQ/OQKLAVZzrAovWS"
+  gistId: "086b5254b5652ccb8e6cc77e37feb6c7",
+  token: "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",
+  fileName: "estado.json"
 };
